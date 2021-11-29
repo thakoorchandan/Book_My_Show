84d5be5d5154622a1a4e2c39064f66279890a5ea
Thakoor Chandan <thakoorchandan@gmail.com>
2021-11-29 16:49:15 +0530

    Fine Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 import All_Movies from "./Components/All_Movies";
 import Home from "./Components/Home";
@@ -11,18 +16,25 @@ import Login from "./Components/Login";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
       <Switch>
         <Route path="/register" exact component={SignUp} />
         <Route path="/login" exact component={Login} />
-        <Route path="/" exact component={Home} />
-        <Route path="/movies" exact={true} component={All_Movies} />
         <Route path="/about" exact={true} component={About} />
         <Route path="/latest-shows" exact={true} component={Latest} />
+
+        {localStorage.getItem("token") ? (
+          <Route exact path="/" component={Home} />
+        ) : (
+          <Redirect to={"/login"} />
+        )}
+
+        <Route path="/" exact component={Home} />
+        <Route path="/movies" exact={true} component={All_Movies} />
         <Route exact path="/movies/:id" component={Movies_id} />
       </Switch>
-    </div>
+    </Router>
   );
 }
 
